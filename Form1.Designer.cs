cf4d602b33e36b16686fb5ffd3e4a30e864bba85
ilya-msoft@mail.ru <ilya-msoft@mail.ru>
2020-02-06 20:48:48 +0500

    значки

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.lblTemp = new System.Windows.Forms.Label();
             this.lblFeels = new System.Windows.Forms.Label();
@@ -231,6 +232,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblTemp);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
